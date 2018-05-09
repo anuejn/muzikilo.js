2736c36dca267d8dc1b7171e0580cf915b2e8a44
Jaro <jarohabiger@googlemail.com>
2018-05-09 15:03:50 +0200

    basic sound synthesis

--- src/App.js
+++ src/App.js
@@ -11,19 +11,63 @@ export default class App extends Component {
     super();
 
     this.state = {
-      
+      fn: () => 0,
+      knobs: {
+        volume: .5,
+        envelope: .5,
+        sustain: .5,
+        decay: .5,
+        distortion: .5,
+      },
+      keys: [],
     }
+
+    this.startAudio();
   }
 
   render() {
     return (
       <SplitterLayout vertical percentage secondaryInitialSize={35}>
         <SplitterLayout percentage secondaryInitialSize={35}>
-          <CodeEditor />
+          <CodeEditor 
+            onChange={newValue => {
+              try {
+                const fnString = `(t, keys, knobs, state) => {\n${newValue}\n}`
+                const fn =  eval();
+                this.setState({fn: fn});
+              } catch(e) {
+                console.warn('code in input did not compile!');
+              }
+            }}
+          />
           <Oszi />
         </SplitterLayout>
-        <Inputs />
+        <Inputs 
+          knobs={this.state.knobs} 
+          onChangeKnobs={newKnobs => this.setState({knobs: newKnobs})}
+
+          keys={this.state.keys}
+          onChangeKeys={newKeys => this.setState({keys: newKeys})}
+        />
       </SplitterLayout>
     );
+  }
+
+
+  startAudio() {
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    const node = audioCtx.createScriptProcessor(1024, 1, 1);
+    window.context = this;
+    const state = {};
+    node.onaudioprocess = function (e) {
+      const {outputBuffer} = e;
+      outputBuffer.copyToChannel(outputBuffer.getChannelData(0).map((_, i) => {
+        const t = (e.playbackTime + i / e.outputBuffer.sampleRate);
+        return window.context.state.fn(t, state);
+      }), 0)
+    };
+
+    node.connect(audioCtx.destination);
   }
 }
--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -1,31 +1,24 @@
 import React from 'react';
-import brace from 'brace';
-import AceEditor from 'react-ace';
-
-import 'brace/mode/javascript';
-import 'brace/theme/github';
-import 'brace/ext/language_tools';
+import MonacoEditor from 'react-monaco-editor';
 
 export class CodeEditor extends React.Component {
-  onChange(newValue) {
-    console.log('change',newValue);
+  constructor() {
+    super();
+    this.state = {
+      code: '// given are t, keys and knobs\nreturn Math.random() * .1'
+    }
   }
-  
-  // Render editor
+
   render() {
-    return <AceEditor
-      mode="javascript"
-      theme="github"
-      onChange={this.onChange}
-      editorProps={{$blockScrolling: true}}
-      setOptions={{
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: false,
-        showLineNumbers: true,
-        tabSize: 2,
-        automaticLayout: true,
-      }}
-    />
-  };
+    return (
+      <MonacoEditor
+        language="javascript"
+        theme="vs-dark"
+        value={this.state.code}
+        onChange={(newValue) => {this.setState({code: newValue}); this.props.onChange(newValue)}}
+        editorDidMount={(editor) => editor.focus()}
+        options={{automaticLayout: true}}
+      />
+    );
+  }
 }
