a570729e62dcf9a068b7244b6a4e5371740d5e25
Philipp Mandler <info@philipp-mandler.com>
2018-06-09 23:13:34 +0200

    webpack: add typescript lang to MonacoWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   plugins: [
     new MonacoWebpackPlugin({
-      languages: [],
+      languages: ['typescript'],
     }),
     new HtmlWebpackPlugin({
       title: 'muzikilo.js',
