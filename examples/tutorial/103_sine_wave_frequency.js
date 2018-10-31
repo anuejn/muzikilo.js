33c770ca27a21808cb7abfa504c05ef1cd8c97dc
Roland Illig <roland.illig@gmx.de>
2018-10-31 11:24:36 +0100

    add tutorial section in the examples
    The tutorial is meant for absolute beginners, both in music and
    programming. It explains the very basic concepts, referring to helpful
    Wikipedia pages for further information.
    
    See https://github.com/anuejn/muzikilo.js/issues/17, which is a feature
    request to add these examples to the web site.

---
+++ examples/tutorial/103_sine_wave_frequency.js
@@ -0,0 +1,45 @@
+// Generates a sine wave.
+// The frequency and volume can be controlled by knobs.
+
+this.t += 1 / 44100
+
+function sin(phi) {
+  return Math.sin(2 * Math.PI * phi)
+}
+
+// Computes a frequency between min and max, depending on the given knob.
+//
+// For a given tone, the tone one octave higher has twice the frequency.
+// Therefore, for a frequency knob, the linear range of the knob with
+// values like 0, 0.25, 0.5, 0.75, 1 is transformed to an exponential
+// range, for example 200, 282, 400, 566, 800.
+function expScale(knob, min, max) {
+  return min * Math.exp(knob * Math.log(max / min))
+}
+
+// The frequency can be adjusted over two octaves.
+//
+// See https://en.wikipedia.org/wiki/Scale_(music)
+// See https://en.wikipedia.org/wiki/C_(musical_note)#Designation_by_octave
+const frequency = expScale(knobs['Frequency'], 261.626, 1046.502)
+
+return knobs['Master volume'] * sin(frequency * t)
+
+// When changing the Frequency knob, the loud speaker makes a crackling noise.
+// This is because the phase of the sine curve may be completely different.
+//
+// As an example, assume that after 3 seconds of playing, the knob changes from
+// 0.50 to 0.49.
+//
+// Knob value    Frequency   Argument to sin   Value of sin
+// -----------------------------------------------------------
+//       0.50   523.251499   1569.754499       -0.999
+//       0.49   516.047751   1548.143255       +0.7833
+//
+// Normally, the sine wave changes very smoothly between -1 and +1. In this
+// case though, the output signal changes abruptly from almost -1 to almost +1.
+// This produces a cracking sound. Combine several of them, and you have an
+// explosion.
+//
+// The follow-up example program will correct this problem by keeping track
+// of the current phase of the sine wave instead of the total elapsed time.
---
+++ examples/tutorial/104_sine_wave_phase.js
@@ -0,0 +1,27 @@
+// Generates a sine wave. The frequency and amplitude can be controlled
+// by knobs. In contrast to the previous example program, changing the
+// frequency doesn't produce a cracking noise.
+
+function sin(phi) {
+  return Math.sin(2 * Math.PI * phi)
+}
+
+function expScale(knob, min, max) {
+  return min * Math.exp(knob * Math.log(max / min))
+}
+
+const frequency = expScale(knobs['Frequency'], 261.626, 1046.502)
+
+// The phase of the sine wave advances depending on the frequency.
+// When changing the frequency via the knob, the phase stays the
+// same, to avoid sudden changes in the output, which would cause
+// a cracking noise.
+//
+// The higher the frequency, the faster the phase changes.
+//
+// The "% 1" at the end makes sure that the phase is always between
+// 0 and 1. This range is convenient for the sine function above,
+// and for many others.
+this.phase = (this.phase + frequency / 44100) % 1
+
+return sin(this.phase) * knobs['Master volume']
